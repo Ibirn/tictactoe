57adefe4ae067260904fbc954a50f1c6968124e7
Ibirn <kmcricciuti@gmail.com>
2020-12-29 16:59:19 -0800

    Add logic gate to prevent played spaces from being reusued and bugfix for removing possible moves

--- tictactoe.js
+++ tictactoe.js
@@ -21,14 +21,24 @@ let gameBoard = {
 };
 let possibleMoves = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"];
 
+const removePossibleMove = (value) => {
+  let index = possibleMoves.indexOf(value);
+  if (index > -1) {
+    possibleMoves.splice(index, 1);
+  }
+  return possibleMoves;
+};
 const drawBoard = () => {
-  rl.write(gameBoard.header + gameBoard.rowA + gameBoard.rowB + gameBoard.rowC);
+  process.stdout.write(
+    gameBoard.header + gameBoard.rowA + gameBoard.rowB + gameBoard.rowC
+  );
 };
 rl.write("Welcome to Tic Tac Toe!\n Are you ready to play?\n");
 drawBoard();
 
 const compTurn = () => {
-  let move = possibleMoves[Math.floor(Math.random() * 9)];
+  console.log("COMPMOVES: ", possibleMoves);
+  let move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
   let temp = 0;
   if (move[1] === "1") {
     temp = 4;
@@ -40,29 +50,42 @@ const compTurn = () => {
     temp = -1;
   }
   gameBoard[`row${move[0]}`] = gameBoard[`row${move[0]}`].replaceAt(temp, "O");
+  removePossibleMove(move);
 };
 
 const turn = () => {
   return new Promise((resolve, reject) => {
     rl.question("Please choose a coordinate: ", (move) => {
-      let temp = 0;
-      if (move[1] === "1") {
-        temp = 4;
-      } else if (move[1] === "2") {
-        temp = 7;
-      } else if (move[1] === "3") {
-        temp = 10;
+      let index = possibleMoves.indexOf(move);
+      console.log("INDEX: ", index, "\nPOSSMO: ", possibleMoves);
+      if (index >= 0) {
+        let temp = 0;
+        if (move[1] === "1") {
+          temp = 4;
+        } else if (move[1] === "2") {
+          temp = 7;
+        } else if (move[1] === "3") {
+          temp = 10;
+        } else {
+          temp = -1;
+        }
+        gameBoard[`row${move[0]}`] = gameBoard[`row${move[0]}`].replaceAt(
+          temp,
+          "X"
+        );
+        removePossibleMove(move);
+        resolve();
+        compTurn();
+        drawBoard();
+        turn();
       } else {
-        temp = -1;
+        process.stdout.write(
+          "That space has already been played. \nPlease choose another.\n"
+        );
+        resolve();
+        drawBoard();
+        turn();
       }
-      gameBoard[`row${move[0]}`] = gameBoard[`row${move[0]}`].replaceAt(
-        temp,
-        "X"
-      );
-      resolve();
-      compTurn();
-      drawBoard();
-      turn();
     });
   });
 };
